1509e688e27d44a53b9e4443fdb473b8f8683614
Abin John <abinjohnanto@gmail.com>
2022-09-10 09:37:30 +0530

    feat: implement customer amount input validation logic

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const billAmountEl = document.getElementById('form-row-bill-amount');
 const userAmountEl = document.getElementById('form-row-user-cash');
 const billAmountValue = document.getElementById('bill-amount');
 const userAmountValue = document.getElementById('user-amount');
+const outputTableEl = [
+  ...document.querySelectorAll('.app-output-change td:not(:nth-child(1))'),
+];
 
 const denominations = [2000, 500, 100, 20, 10, 5, 1];
 
@@ -19,7 +22,7 @@ function displayError(code = -1) {
 }
 
 function billAmountValidator(e) {
-  const value = e.target.value;
+  const value = Number(e.target.value);
   if (value <= 0 || value === '') {
     displayError(0);
     userAmountValue.disabled = true;
@@ -32,27 +35,39 @@ function billAmountValidator(e) {
   userAmountValue.placeholder = '';
 }
 
+function inputAmountValidator(e) {
+  const value = Number(e.target.value);
+  if (value <= Number(billAmountValue.value)) {
+    displayError(1);
+    return;
+  }
+  displayError(-1);
+}
+
 function calculateChange(e) {
   e.preventDefault();
   const billAmount = Number(billAmountValue.value);
   const userAmount = Number(userAmountValue.value);
-
-  console.log(billAmount, userAmount);
 
   let balance = userAmount - billAmount;
 
   const notes = denominations.map((note) => {
     const numNotes = Math.floor(balance / note);
     if (numNotes > 0) balance = Math.floor(balance % note);
-    return numNotes;
+    return numNotes > 0 ? numNotes : '-';
   });
-  console.log(notes);
+
+  notes.reverse().forEach((note, i) => {
+    outputTableEl[i].innerText = note;
+  });
 }
 
 // Event Listeners
 billAmountValue.addEventListener('input', billAmountValidator);
+userAmountValue.addEventListener('input', inputAmountValidator);
 form.addEventListener('submit', calculateChange);
 
 //Init
 userAmountValue.disabled = true;
 userAmountValue.placeholder = 'Enter a valid bill amount first';
+console.log(outputTableEl);
