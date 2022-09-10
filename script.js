23b7f6e4472fd5af83102f10e2922dc09bed5eeb
Abin John <abinjohnanto@gmail.com>
2022-09-10 18:57:31 +0530

    fix: input validation on form submit

--- script.js
+++ script.js
@@ -12,25 +12,29 @@ const outputTableEl = [
 
 const denominations = [2000, 500, 100, 20, 10, 5, 1];
 
+function clearOutputFields() {
+  outputTableEl.forEach((cell) => (cell.innerText = '-'));
+  balanceEl.innerText = '0';
+}
+
 function displayError(code = -1) {
-  function returnErrorElement(message) {
+  function returnErrorHTML(message) {
     return `<small class="error-message">${message}</small>`;
   }
-  const errorMessage = returnErrorElement;
+
   if (code === 0) {
     if (!billAmountValue.classList.contains('error'))
       billAmountEl.insertAdjacentHTML(
         'beforeend',
-        returnErrorElement('Bill Amount must be greater than 0')
+        returnErrorHTML('Bill Amount must be greater than 0')
       );
 
     billAmountValue.classList.add('error');
   } else if (code === 1) {
-    console.log('code 1');
     if (!userAmountValue.classList.contains('error'))
       userAmountEl.insertAdjacentHTML(
         'beforeend',
-        returnErrorElement('Cash Received must be greater than bill amount')
+        returnErrorHTML('Cash Received must be greater than bill amount')
       );
 
     userAmountValue.classList.add('error');
@@ -42,31 +46,43 @@ function displayError(code = -1) {
   }
 }
 
-function billAmountValidator(e) {
-  const value = Number(e.target.value);
+function billAmountValidator() {
+  // clear previous change info
+  clearOutputFields();
+
+  const value = Number(billAmountValue.value);
   if (value <= 0 || value === '') {
     displayError(0);
     userAmountValue.disabled = true;
-    userAmountValue.placeholder = 'Enter a valid bill amount first';
-    return;
+    userAmountValue.placeholder = 'Enter bill amount first';
+    return false;
   }
 
   displayError(-1);
   userAmountValue.disabled = false;
   userAmountValue.placeholder = '';
+  return true;
 }
 
-function inputAmountValidator(e) {
-  const value = Number(e.target.value);
+function inputAmountValidator() {
+  // clear previous change info
+  clearOutputFields();
+
+  const value = Number(userAmountValue.value);
   if (value <= Number(billAmountValue.value)) {
     displayError(1);
-    return;
+    return false;
   }
   displayError(-1);
+  return true;
 }
 
 function calculateChange(e) {
   e.preventDefault();
+
+  // Guard clause to prevent change calculation for incorrect inputs
+  if (!billAmountValidator() || !inputAmountValidator()) return;
+
   const billAmount = Number(billAmountValue.value);
   const userAmount = Number(userAmountValue.value);
 
@@ -91,5 +107,5 @@ form.addEventListener('submit', calculateChange);
 
 //Init
 userAmountValue.disabled = true;
-userAmountValue.placeholder = 'Enter a valid bill amount first';
+userAmountValue.placeholder = 'Enter bill amount first';
 console.log(outputTableEl);
